f45d84381f663cc8013e6f78d13fb8d699e1c7c7
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-05-21 23:49:09 +0200

    added font family and first styles to file

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,15 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     html {
-        box-sizing: border-box;
+      box-sizing: border-box;
     }
     *, *::before, *::after {
-        box-sizing: inherit;
+      box-sizing: inherit;
     }
     body {
-        margin: 0 50px;
-        background-color: gray;
+      font-family: "Montserrat", sans-serif;
+      margin: 0 50px;
+      background-color: #EFEEE5;
     }
 `;
 
 export default GlobalStyle;
+
---
+++ src/styled.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    
+`;
+
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const StyledButton = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  margin: 5px;
+`;
+
+export const Header = styled.h1`
+    text-align: center;
+    font-size: 3rem;
+    margin: .5rem;
+`;
+
+export const ImageContainer = styled.div`
+    overflow: hidden;
+`;
+
+export const Image = styled.img`
+max-height: 300px;
+aspect-ratio: 16 / 9;
+width: 100%;
+object-fit: cover;
+object-position: center;
+display: block;
+`;
+
+export const Tile = styled.div`
+  list-style: none;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProductsGridContainer = styled.div`
+  margin-top: .5rem;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+`;
