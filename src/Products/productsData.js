556f13287f3db7f2b95d9d8ef2fdd9b15cb90b63
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-05-24 20:55:52 +0200

    created array objects with products data

---
+++ src/Products/productsData.js
@@ -0,0 +1,31 @@
+import blackClassicTshirt from "../images/blackClassicTshirt.jpg";
+import blackDobermanTshirt from "../images/blackDobermanTshirt.jpg";
+import whiteClassicTshirt from "../images/whiteClassicTshirt.jpg";
+import whiteDobermanTshirt from "../images/whiteDobermanTshirt.jpg";
+
+export const productsData = [
+  {
+    id: 1,
+    name: "Czarny T-shirt klasyczny",
+    image: blackClassicTshirt,
+    price: "99,00zł"
+  },
+  {
+    id: 2,
+    name: "Czarny T-shirt Doberman",
+    image: blackDobermanTshirt,
+    price: "99,00zł"
+  },
+  {
+    id: 3,
+    name: "Biały T-shirt klasyczny",
+    image: whiteClassicTshirt,
+    price: "99,00zł"
+  },
+  {
+    id: 4,
+    name: "Biały T-shirt Doberman",
+    image: whiteDobermanTshirt,
+    price: "99,00zł"
+  },
+];
