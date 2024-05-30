2a4d53138f1cd9b0c6673116db5550f33ce14eb5
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-05-31 00:46:33 +0200

    created file with routes to use them later in project

---
+++ src/core/App/routes.js
@@ -0,0 +1,5 @@
+// export const toProduct = () => "/produkt"
+export const toRegulations = () => "/regulamin";
+export const toRefund = () => "/zwroty-reklamacje";
+export const toContact = () => "/kontakt";
+export const toPolicePrivacy = () => "/polityka-prywatności";
