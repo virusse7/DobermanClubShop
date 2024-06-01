56d6806991056f5fe49a279dce76d675138c6818
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-06-01 14:08:22 +0200

    created test subpage

---
+++ src/features/Contact/index.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export const Contact = () => (
+  <>
+    <div>
+      Hello
+    </div>
+    <Link to={`/DobermanClubShop`}>
+      Powrót
+    </Link>
+  </>
+)
