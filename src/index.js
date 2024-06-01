b708d3ba76e2cea878449d937af73206d853ee06
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-06-01 14:07:55 +0200

    created paths to subpages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './core/App';
-import GlobalStyle from './core/App/GlobalStyle';
-import { Normalize } from 'styled-normalize';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Contact } from './features/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/DobermanClubShop",
+    element: <App />,
+  },
+  {
+    path: "/DobermanClubShop/kontakt",
+    element: <Contact />
+
+  },
+  // {
+  //   path: "/DobermanClubShop/regulamin",
+  //   element: <Regulations />
+  // },
+  // {
+  //   path: "/DobermanClubShop/zwroty-reklamacje",
+  //   element: <Refund />
+  // },
+  // {
+  //   path: "/DobermanClubShop/polityka-prywatnosci",
+  //   element: <PolicePrivacy />
+  // }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Normalize />
-    <GlobalStyle />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
