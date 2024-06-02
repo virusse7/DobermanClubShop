826a103719c64ffe270c9ac3a36bcada0aed5076
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-06-02 18:33:57 +0200

    moved globalstyle and normalize to correct folder

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { DobermanClubShop } from '../../features/DobermanClubShop';
-import GlobalStyle from './GlobalStyle';
-import { Normalize } from 'styled-normalize';
+
 
 export const App = () => {
   return (
     <React.StrictMode>
-      <Normalize />
-      <GlobalStyle />
       <DobermanClubShop />
     </React.StrictMode>
   );
